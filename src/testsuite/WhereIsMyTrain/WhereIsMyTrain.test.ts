fc2929f446665caed7e323fa32213d88ea233115
ibswapnilsananse <swapnil.sananse@infobeans.com>
2026-06-15 16:41:54 +0530

    SCA fixes

--- src/testsuite/WhereIsMyTrain/WhereIsMyTrain.test.ts
+++ src/testsuite/WhereIsMyTrain/WhereIsMyTrain.test.ts
@@ -17,7 +17,7 @@ describe("Where Is My Train - Test Suite", function () {
     await trainHelper.verifyHomePageDisplayed();
   });
 
-  it("TC01: Verify Home Page is displayed", async function () {
+  it("TC01: Verify Home Page is displayed [MYY-2]", async function () {
     const elements = await trainHelper.verifyHomePageElements();
     expect(elements.home).to.be.true;
     expect(elements.from).to.be.true;
@@ -25,27 +25,27 @@ describe("Where Is My Train - Test Suite", function () {
     expect(elements.findTrains).to.be.true;
   });
 
-  it("TC02: Search trains From New Delhi To Mumbai CSMT and verify results", async function () {
+  it("TC02: Search trains From New Delhi To Mumbai CSMT and verify results [MYY-3]", async function () {
     await trainHelper.searchTrainsByFromTo("New Delhi", "NDLS", "Mumbai CSMT", "CSMT");
     const { trainNumbers, sourceStations } = await trainHelper.getFromToSearchResults();
     expect(trainNumbers.length).to.be.greaterThan(0);
     expect(sourceStations.length).to.be.greaterThan(0);
   });
 
-  it("TC03: Search train by number 22308 and verify results on SearchByTrainNoTrainName page", async function () {
+  it("TC03: Search train by number 22308 and verify results on SearchByTrainNoTrainName page [MYY-4]", async function () {
     await trainHelper.searchByTrainNumber("22308");
     const trainNumbers = await trainHelper.getTrainChooserResults();
     expect(trainNumbers.length).to.be.greaterThan(0);
     expect(trainNumbers.some((n) => n.includes("22308"))).to.be.true;
   });
 
-  it("TC04: Search trains by station New Delhi and verify results on SearchResultsForTrains page", async function () {
+  it("TC04: Search trains by station New Delhi and verify results on SearchResultsForTrains page [MYY-5]", async function () {
     await trainHelper.searchByStation("New Delhi", "NDLS");
     const trainNumbers = await trainHelper.getStationSearchResults();
     expect(trainNumbers.length).to.be.greaterThan(0);
   });
 
-  it("TC05: Select a train from SearchResultsForTrains and verify TrainStatusView page", async function () {
+  it("TC05: Select a train from SearchResultsForTrains and verify TrainStatusView page [MYY-6]", async function () {
     await trainHelper.searchByStation("New Delhi", "NDLS");
     const selectedTrainNumber = await trainHelper.selectFirstTrainFromResults();
     const headingText = await trainHelper.getTrainStatusHeading();
@@ -53,7 +53,7 @@ describe("Where Is My Train - Test Suite", function () {
     expect(headingText).to.include("-");
   });
 
-  it("TC06: Verify recent search appears on the Home screen after searching by train number", async function () {
+  it("TC06: Verify recent search appears on the Home screen after searching by train number [MYY-7]", async function () {
     await trainHelper.searchByTrainNumber("22308");
     await trainHelper.selectFirstTrainInChooserAndGoBack();
     const historyEntries = await trainHelper.getSearchHistoryEntries();
